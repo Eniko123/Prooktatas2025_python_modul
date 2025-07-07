24c01a26f992fbec86009e45cbca46c9b4928398
Eniko123 <94745220+Eniko123@users.noreply.github.com>
2025-07-07 18:08:15 +0200

    Add files via upload

---
+++ tarieniko_om_56ora.py
@@ -0,0 +1,92 @@
+import random
+from os import remove
+
+
+def elso_feladat():
+    """
+    Ez egy olyan függvény, a listában lévő számok közül a párosokat 3-al szorozza, a páratlanokat 5-el.
+    valamint minden ötödik elemet kihagy.
+    """
+
+    rand_list = [i * 3 if i % 2 == 0 else i * 5
+        for i in rand_list:
+            if (i+1) % 5 == 0:
+                remove()]
+
+    rand_list_length = 42
+    for random_num in range(rand_list_length):
+        rand_list.append(random.randint(1, 100))
+    i = 0
+    for i in rand_list:
+        if i  % 2 == 0
+            return i * 3
+        if i % 2 != 0:
+            return i * 5
+        remove(if (i+1) % 5 == 0)
+
+    print(rand_list)
+
+    rand_list.append[1](print(int(input("Írj be egy számot amit beszúrnál a listába: "))))
+    print(rand_list)
+
+    rand_list.remove(input("Melyik elemet szeretnéd törölni? "))
+    print(rand_list)
+
+    sorted(rand_list)
+    print(rand_list)
+
+    sorted(rand_list.reverse)
+    print(rand_list)
+
+
+
+# Második feladat segéd függvények
+def newmem():
+    pass
+
+def delete():
+
+
+def modify():
+    pass
+
+def end():
+    while print(input("Ha végeztél: q, end, quit")) != "q" or "Q":
+        for key, value in tanulok:
+            print(f'{key}:{value}')
+
+def masodik_feladat():
+    """
+    Ez a függvény kiírja a szótár kulcs-érték párjait.
+    """
+
+    tanulok = [{"nev": "Teszt Elek", "osztaly": "13I", "eletkor": 19}, {"nev": "Kiss Béla", "osztaly": "12A",
+                                                                        "eletkor": 18},
+               {"nev": "Nagy Anna", "osztaly": "11B", "eletkor": 17}, {"nev": "Szabó Gergő", "osztaly": "10C",
+                                                                       "eletkor": 16},
+               {"nev": "Varga László", "osztaly": "9D", "eletkor": 15}, {"nev": "Tóth Zsófia", "osztaly": "12A",
+                                                                         "eletkor": 18}]
+
+    tovabb = print("Új tag – newmem | Törlés – delete | Módosítás – modify | Kilépés – end, Q, quit| Mit szeretne csinálni? ")
+
+    if tovabb == newman:
+        newman(tanulok)
+    elif tovabb == delete:
+        delete(tanulok)
+    elif tovabb == modify:
+        modify(tanulok)
+    elif tovabb == quit:
+        end(tanulok)
+    else
+        print("Ajon meg más bementetet")
+
+
+
+
+
+
+
+
+
+    def harmadik_feladat():
+    pass
